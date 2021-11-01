826aff4de99146c5b7e68732a0d17fbaa760ccca
whutliqi <whutliqi@gmail.com>
2021-11-01 20:44:39 +0800

    second commit

---
+++ article/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+app_name = 'article'
+
+urlpatterns = [
+    path('article-list/', views.article_list, name='article_list'),
+    # 文章详情
+    path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
+    # 写文章
+    path('article-create/', views.article_create, name='article_create'),
+]
